dcdd863bb1f869bfdb114b6771b8ddc5ccb15315
Justin-Bee <jbee.appz@gmail.com>
2019-10-08 19:06:20 -0400

    working on task #48

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,23 +1,45 @@
+/**
+ * @author: Justin Bee
+ * @date: 10/8/2019
+ *
+ */
+
+
 //set event listener to the editor for keyup
 document.getElementById('editor').addEventListener("keyup", updateEditor, false);
+
+const keyWords = ['import', 'from', 'if', 'else', 'for', 'while'];
 /*
-function to
+function to highlight the syntax for MicroPython
  */
 function updateEditor(){
-    //get the users input
+    //get the users input from the <div>
     var divValue = document.getElementById('editor').innerHTML;
-    //if
-    if(divValue.includes("import")){
-    var replace = divValue.replace(/import/g, "<span style=color:orange;>import</span></span>");
-    document.getElementById('editor').innerHTML=replace;
-    //set the cursor at the end
-    var el = document.getElementById('editor');
-    var selection = window.getSelection();
-    var range = document.createRange();
-    selection.removeAllRanges();
-    range.selectNodeContents(el);
-    range.collapse(false);
-    selection.addRange(range);
-    el.focus();
+    var found = false;
+    //for loop to search the input
+    for(var i=0; i<=keyWords.length; i++) {
+        //if to replace with highlighting
+        if (divValue.includes(keyWords[i])) {
+            //create regex variable
+            var word = keyWords[i].toString();
+            var re = new RegExp(word, "g");
+            var replace = divValue.replace(re, "<span style=color:orange;>" + word + "</span></span>");
+            document.getElementById('editor').innerHTML = replace;
+            cursorAtEnd();
+        }
     }
+}
+/**
+ * this function sets the cursor at the end of the text
+ */
+function cursorAtEnd(){
+    //set the cursor at the end of text in div
+            var el = document.getElementById('editor');
+            var selection = window.getSelection();
+            var range = document.createRange();
+            selection.removeAllRanges();
+            range.selectNodeContents(el);
+            range.collapse(false);
+            selection.addRange(range);
+            el.focus();
 }
