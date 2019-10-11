7b5bef614f9d18d1177c2c7a71d3282a7df324c7
Justin-Bee <jbee.appz@gmail.com>
2019-10-11 15:16:34 -0400

    US#56 and bug fixes

--- static/js/editor.js
+++ static/js/editor.js
@@ -6,12 +6,14 @@
 
 
 //set event listener to the editor for keyup
-document.getElementById('editor').addEventListener("keyup", highlightKeywords, false);
+document.getElementById('editor').addEventListener("keydown", highlightKeywords, false);
 
 const keyWords = ['import', 'from', 'if', 'else', 'for', 'while', 'False', 'None', 'True',
                     'and', 'as', 'assert', 'break', 'class', 'continue', 'def','del', 'elif',
                     'except', 'finally', 'global', 'in', 'is', 'lambda', 'nonlocal', 'not',
                     'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield'];
+
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 /**
  * TODO write a config function so we can change the colors of the formatting based upon the styles
@@ -20,34 +22,61 @@ const keyWords = ['import', 'from', 'if', 'else', 'for', 'while', 'False', 'None
 
 
 /**
-function to highlight the syntax for MicroPython
+ * @author: Justin Bee
+ * @param: none
+ * function to highlight the syntax for MicroPython
  */
 function highlightKeywords() {
-     //have to set a delay so it doesn't format a partial word
-    //TODO find an optimal delay
-     setTimeout(function(){
-        var divValue = document.getElementById('editor').innerHTML;
+        var divValue = document.getElementById('editor').innerText;
         for(var i=0; i<keyWords.length; i++) {
             var word = keyWords[i];  //word we want to replace
-            var wordHTML = "&gt;"+keyWords[i]+"&lt;";  //variable for same word surrounded by html tags
+            var wordHTML = "&gt;"+word+"&lt;";  //variable for same word surrounded by html tags
             if (divValue.includes(wordHTML)){
                 //do nothing so we dont continue to wrap the text in html tags
                 //TODO maybe explore this more with the HTML tags
-            }else if (divValue.includes(word)) {
+            }else if(divValue.includes(word)) {
                 //create regex variable
                 re = new RegExp('\\b'+word+'\\b',"g");
                 divValue = divValue.replace(re , "<span style=color:orange>" + word+ "</span></span>");
                 document.getElementById('editor').innerHTML = divValue;
                 cursorAtEnd();
-                console.log(divValue);
+               // console.log(document.getElementById('editor').innerHTML);
             }
         }
-      },2000); //1.5 second delay
-
+        highlightNumbers();
 }
 
 /**
+ * @author: Justin Bee
+ * @param: none
+ * The function finds numeric characters and highlights them
+ */
+function highlightNumbers(){
+    var divValue = document.getElementById('editor').innerHTML;
+    //    var divValue = divValue.toString();
+        for(var i=0; i<=numbers.length; i++) {
+            var num = numbers[i];  //word we want to replace
+            var numHTML = "&gt;"+num+"&lt;";  //variable for same word surrounded by html tags
+            if (divValue.includes(numHTML)){
+                //do nothing so we dont continue to wrap the text in html tags
+                //TODO maybe explore this more with the HTML tags
+            }else if(divValue.includes(num)) {
+                //create regex variable
+                //TODO need to improve upon this 
+                //if user enters xxxx8888 the 8888 will still get highlighted.
+                re = new RegExp(num, "g");
+                divValue = divValue.replace(re , "<span style=color:blue>" + num+ "</span></span>");
+                document.getElementById('editor').innerHTML = divValue;
+                console.log(divValue);
+                cursorAtEnd();
+            }
+        }
+}
+
+
+/**
  * this function sets the cursor at the end of the text
+ * TODO fix fnction to find current location of cursor and set back to there
  */
 function cursorAtEnd(){
     //set the cursor at the end of text in div
@@ -60,6 +89,8 @@ function cursorAtEnd(){
             selection.addRange(range);
             el.focus();
 }
+
+
 
 
 
