afb555702db104702723d307b9d5e0bc9c808ac8
Justin-Bee <jbee.appz@gmail.com>
2019-10-13 12:43:04 -0400

    US#56 working on number fucntion

--- static/js/editor.js
+++ static/js/editor.js
@@ -5,15 +5,14 @@
  */
 
 
-//set event listener to the editor for keyup
-document.getElementById('editor').addEventListener("keydown", highlightKeywords, false);
+//set event listener to the editor for keypress
+document.getElementById('editor').addEventListener("keypress", highlightKeywords, false);
 
 const keyWords = ['import', 'from', 'if', 'else', 'for', 'while', 'False', 'None', 'True',
                     'and', 'as', 'assert', 'break', 'class', 'continue', 'def','del', 'elif',
                     'except', 'finally', 'global', 'in', 'is', 'lambda', 'nonlocal', 'not',
                     'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield'];
 
-const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 /**
  * TODO write a config function so we can change the colors of the formatting based upon the styles
@@ -40,10 +39,11 @@ function highlightKeywords() {
                 divValue = divValue.replace(re , "<span style=color:orange>" + word+ "</span></span>");
                 document.getElementById('editor').innerHTML = divValue;
                 cursorAtEnd();
-               // console.log(document.getElementById('editor').innerHTML);
+                console.log(document.getElementById('editor').innerHTML);
             }
         }
         highlightNumbers();
+        cursorAtEnd();
 }
 
 /**
@@ -52,31 +52,27 @@ function highlightKeywords() {
  * The function finds numeric characters and highlights them
  */
 function highlightNumbers(){
-    var divValue = document.getElementById('editor').innerHTML;
-    //    var divValue = divValue.toString();
-        for(var i=0; i<=numbers.length; i++) {
-            var num = numbers[i];  //word we want to replace
-            var numHTML = "&gt;"+num+"&lt;";  //variable for same word surrounded by html tags
-            if (divValue.includes(numHTML)){
-                //do nothing so we dont continue to wrap the text in html tags
-                //TODO maybe explore this more with the HTML tags
-            }else if(divValue.includes(num)) {
-                //create regex variable
-                //TODO need to improve upon this 
-                //if user enters xxxx8888 the 8888 will still get highlighted.
-                re = new RegExp(num, "g");
-                divValue = divValue.replace(re , "<span style=color:blue>" + num+ "</span></span>");
-                document.getElementById('editor').innerHTML = divValue;
-                console.log(divValue);
-                cursorAtEnd();
-            }
-        }
+     var divValue = document.getElementById('editor').innerHTML;
+   //  wsReg = new RegExp('&nbsp;', 'g');
+  //   divValue = divValue.replace(wsReg, " ");
+     var str = divValue.split(/[\s|\u00A0]/);
+     for(var i =0; i<str.length; i++) {
+          console.log(str[i]);
+         if(str[i].match(/^[0-9]+$/)){
+             str[i]= "<span style=color:blue>"+str[i]+"</span></span>";
+         }
+     }
+     divValue = "";
+     for(var i =0; i<str.length; i++){
+         divValue = divValue + str[i] + " ";
+     }
+     document.getElementById('editor').innerHTML = divValue;
 }
 
 
 /**
  * this function sets the cursor at the end of the text
- * TODO fix fnction to find current location of cursor and set back to there
+ * TODO fix function to find current location of cursor and set back to there
  */
 function cursorAtEnd(){
     //set the cursor at the end of text in div
@@ -89,6 +85,9 @@ function cursorAtEnd(){
             selection.addRange(range);
             el.focus();
 }
+
+
+
 
 
 
