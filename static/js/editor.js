c1a8325e95fbdbe84aecbf39d4c901e0d0e7b23f
Justin-Bee <jbee.appz@gmail.com>
2019-10-16 19:58:33 -0400

    added license

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,7 +1,22 @@
 /**
- * @author: Justin Bee
- * @date: 10/8/2019
+ * Copyright [2019] [Justin Bee, Brian Carson, Andrew Fiorentino, Abigail Ida, Vicente Ochoa]
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Purpose: This file handles the formatting of the code editor
+ * SER401
+ * @file editor.js
+ * @author Justin Bee
+ * @version October 2019
  */
 
 
@@ -28,10 +43,10 @@ editor.addEventListener("keydown", function(event){
         if (window.getSelection) {
              var selection = window.getSelection(),
              range = selection.getRangeAt(0),
-             br = document.createElement("br"),
+             br = document.createElement("br"), //create <br> for the html
              textNode = document.createTextNode("\u00a0"); //Passing " " directly will not end up being shown correctly
-             range.deleteContents();//required or not?
-             range.insertNode(br);
+             range.deleteContents();
+             range.insertNode(br); //insert the br into the correct position
              range.collapse(false);
              range.insertNode(textNode);
              range.selectNodeContents(textNode);
