8cf561c758c4f4fd99416b909a531b586aad6156
Andrew Fiorentino <ajfioren@asu.edu>
2019-10-17 17:49:25 -0700

    US#43-- add load functionality to load button

--- static/js/loadFile.js
+++ static/js/loadFile.js
@@ -1,13 +1,21 @@
 function loadFile() {
-    var fileupload = document.getElementById("FileUpload1");
+    var fileupload = document.getElementById("file");
     var filePath = document.getElementById("spnFilePath");
     var button = document.getElementById("loadFileBtn");
     button.onclick = function () {
         fileupload.click();
     };
-    fileupload.onchange = function () {
-        //var fileName = fileupload.value.split('\\')[fileupload.value.split('\\').length - 1];
-        //filePath.innerHTML = "<b>Selected File: </b>" + fileName;
-        document.getElementById('editor').innerHTML = fileupload;
+    file.onchange = function () {
+        var projectName = fileupload.value.split('\\')[fileupload.value.split('\\').length - 1];
+
+        var file = this.files[0];
+
+        var reader = new FileReader();
+        reader.onload = function(progressEvent){
+            document.getElementById('editor').innerHTML = this.result;
+        };
+        reader.readAsText(file);
+        document.getElementById('editor').setAttribute('name', projectName);
     };
+
 }
