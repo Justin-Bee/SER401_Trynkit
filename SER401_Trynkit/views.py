614f22b01e566fedf0cbee7e2379da9b12fe6833
vicente8a <vicente8alorenzo@gmail.com>
2020-02-06 12:43:48 -0800

    Update views.py
    playing around with login

--- SER401_Trynkit/views.py
+++ SER401_Trynkit/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # @author: Justin Bee, Brian Carson
 # @date: 10/6/2019
 
+@login_required(login_url='/accounts/login/')
 def index(request):
     return render(request, 'index.html')
 
