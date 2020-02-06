9a84c0cc68d95c680a9eeaf8ecc73a34343f1515
vicente8a <vicente8alorenzo@gmail.com>
2020-02-06 12:43:02 -0800

    Update urls.py
    playing around with login DB

--- SER401_Trynkit/urls.py
+++ SER401_Trynkit/urls.py
@@ -21,9 +21,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-
+from django.conf.urls import include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
