f8fcbe9b69052ab3a5ee8f6d304f684833135666
Justin-Bee <jbee.appz@gmail.com>
2020-02-08 16:38:57 -0500

    DB test

--- Trynkit/models.py
+++ Trynkit/models.py
@@ -6,7 +6,12 @@
 
 from django.db import models
 
+
 class User(models.Model):
-    username = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, primary_key=True, unique=True)
     password = models.CharField(max_length=50)
-    email = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.username
+
--- Trynkit/views.py
+++ Trynkit/views.py
@@ -1,3 +1,44 @@
+
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import User
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def devices(request):
+    return render(request, 'devices.html')
+
+
+def console(request):
+    return render(request, 'console.html')
+
+
+def create_user(request):
+    posts = User.objects.all()
+    response_data = {}
+    if request.POST.get('action') == 'post':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+
+        response_data['username'] = username
+        response_data['password'] = password
+        response_data['email'] = email
+
+        User.objects.create(
+            username = username,
+            password = password,
+            email = email,
+        )
+
+
+
+        return JsonResponse(response_data)
+
+    return render(request, 'index.html', {'posts': posts})
