9299c9964723f3845fca0cce6c96cb18091bc774
Justin-Bee <jbee.appz@gmail.com>
2020-02-13 18:55:13 -0500

    US#280 started to work on the send file functionality

--- static/js/bluetooth.js
+++ static/js/bluetooth.js
@@ -1,9 +1,21 @@
 
 /* constant for the service UUID */
 const service = 0x1825;
+/* constant for the TX characteristic */
+const TX_char =  '30ff6dae-fbfe-453b-8a99-9688fea23832';
+let TX_characteristic;
+/* constant for the RX characteristic */
+const RX_char = 'fbdf3e86-c18c-4e5b-aace-e7cc03257f7c';
+let RX_characteristic;
 /* variable so we can tell if the device is connected */
 var isConnected = 0;
 
+/* variable for the device */
+let device;
+/* variable for the ble server */
+let bleserver;
+/* variable for the object transfer service */
+let objservice;
 /*
  * bluetooth()
  * Function for the web bluetooth functionality
@@ -13,6 +25,9 @@ var isConnected = 0;
 
 function bluetooth(){
 
+     if (!navigator.bluetooth) {
+        return alert('Web Bluetooth API is not available in this browser. Please use chrome.');
+     }
      const z = document.getElementById('debugger');
 
      if (z.style.display ==="none"){
@@ -32,9 +47,14 @@ function bluetooth(){
      z.innerHTML= z.innerHTML +"\n"+  (">Name:" + device.name);
      z.innerHTML= z.innerHTML + "\n"+ (">id:" + device.id);
      isConnected = 1;
-    // return device.gap.connect();
-
+     device = device;
      })
+         .then(server=>{
+             bleserver = server.getPrimaryService(service);
+         })
+         .then(service =>{
+           //  objservice = service.get
+         })
      .catch(error=> {
      z.innerHTML= z.innerHTML + "\n"+ (error);
      });
@@ -48,12 +68,28 @@ function bluetooth(){
  * Author: Justin Bee
  */
 function bleSend() {
-     /* check if the device is connected if true send file*/
-     if(isConnected){
+    /* check if the device is connected if true send file*/
+    let encoder = new TextEncoder('utf-8');
+    //  TX_characteristic.writeValue(encoder.encode('value'));
+    navigator.bluetooth.requestDevice({
+     filters:[{
+     name: 'MicroTrynkit'
+          }]
+    })
+     .then(device=>device.gatt.connect())
+        .then(server=>{
+             bleserver = server.getPrimaryService(service);
+            })
+         .then(service =>{
+            TX_characteristic = service.getCharacteristic(TX_char);
+            RX_characteristic = service.getCharacteristic(RX_char);
+            RX_characteristic.writeValue(encoder.encode('value'));
+         })
+     .catch(error=> {
+     z.innerHTML= z.innerHTML + "\n"+ (error);
+     });
 
-     }else{
-        alert("MicroTrynkit device not connected. Please pair it first.");
-    }
+
 }
 
 /*
