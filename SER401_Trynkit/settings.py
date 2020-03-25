6af16bf5b3c9195f88a9079d46b548c8c2084bcc
vicente8a <vicente8alorenzo@gmail.com>
2020-03-25 16:48:49 -0700

    Update settings.py
    email send

--- SER401_Trynkit/settings.py
+++ SER401_Trynkit/settings.py
@@ -94,6 +94,20 @@ DATABASES = {
     }
 }
 
+# send email
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_USE_TLS = True
+#EMAIL_PORT = 587
+#EMAIL_HOST_USER = 'vdochoa.asu@gmail.com'
+#EMAIL_HOST_PASSWORD = 'Toquito.2020'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'vdochoa.asu@gmail.com'
+EMAIL_HOST_PASSWORD = 'Toquito.2020'
+EMAIL_PORT = 587
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
